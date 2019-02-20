2fd898466540fb962b875a6d6c77f8a0fd063be8
Albert Stjärne <albert.stjarne@gmail.com>
2019-02-20 11:28:36 +0100

    adds catch to display error and removing of top-bar

--- js/main.js
+++ js/main.js
@@ -75,7 +75,16 @@ pdfjsLib.getDocument(url).promise.then(pdfDoc_ => {
   document.querySelector('#page-count').textContent = pdfDoc.numPages;
 
   renderPage(pageNum)
-});
+})
+  .catch(err => {
+    // Display error
+    const div = document.createElement('div');
+    div.className = 'error';
+    div.appendChild(document.createTextNode(err.message));
+    document.querySelector('body').insertBefore(div, canvas);
+    // Remove top bar
+    document.querySelector('.top-bar').style.display = 'none';
+  });
 
 // Button events, prev next page
 document.querySelector('#prev-page').addEventListener('click', showPrevPage);
